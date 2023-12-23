1bcbc85f077ffaf203e954bba4c85851e26b6f71
zjyzip <zjyzip@github.com>
2023-12-23 20:43:32 +0800

    fix request list sortrt.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         getByName("debug") {
