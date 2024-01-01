0dd714461c52f5b70ea2cd357b971cd019702a83
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-01-01 12:51:25 +0800

    feat: [wip] show request params

--- app/src/main/java/com/close/hook/ads/ui/adapter/BlockedRequestsAdapter.kt
+++ app/src/main/java/com/close/hook/ads/ui/adapter/BlockedRequestsAdapter.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.close.hook.ads.R
 import com.close.hook.ads.data.model.BlockedRequest
 import com.close.hook.ads.util.AppUtils
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -30,12 +29,18 @@ class BlockedRequestsAdapter(
 
     private val DIFF_CALLBACK: DiffUtil.ItemCallback<BlockedRequest> =
         object : DiffUtil.ItemCallback<BlockedRequest>() {
-            override fun areItemsTheSame(oldItem: BlockedRequest, newItem: BlockedRequest): Boolean {
+            override fun areItemsTheSame(
+                oldItem: BlockedRequest,
+                newItem: BlockedRequest
+            ): Boolean {
                 return oldItem.hashCode() == newItem.hashCode()
             }
 
             @SuppressLint("DiffUtilEquals")
-            override fun areContentsTheSame(oldItem: BlockedRequest, newItem: BlockedRequest): Boolean {
+            override fun areContentsTheSame(
+                oldItem: BlockedRequest,
+                newItem: BlockedRequest
+            ): Boolean {
                 return oldItem.hashCode() == newItem.hashCode()
             }
         }
@@ -51,6 +56,23 @@ class BlockedRequestsAdapter(
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.blocked_request_item, parent, false)
         val viewHolder = ViewHolder(view)
+        viewHolder.itemView.setOnClickListener {
+            MaterialAlertDialogBuilder(parent.context).apply {
+                setTitle("请求参数")
+                setMessage(
+                    """
+method: ${viewHolder.method}
+urlString = ${viewHolder.urlString}
+requestHeaders = ${viewHolder.requestHeaders}
+responseCode = ${viewHolder.responseCode}
+responseMessage = ${viewHolder.responseMessage}
+responseHeaders = ${viewHolder.responseHeaders}
+                """.trimIndent()
+                )
+                setPositiveButton("关闭", null)
+                show()
+            }
+        }
         viewHolder.itemView.setOnLongClickListener {
             val clipboardManager =
                 parent.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
@@ -63,19 +85,35 @@ class BlockedRequestsAdapter(
         return viewHolder
     }
 
-    @SuppressLint("RestrictedApi")
+    @SuppressLint("RestrictedApi", "SimpleDateFormat")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val request = differ.currentList[position]
         holder.appName.text = request.appName
         holder.request.text = request.request
-        if (request.requestType =="all" && request.isBlocked == true) {
-            holder.request.setTextColor(ThemeUtils.getThemeAttrColor(context, com.google.android.material.R.attr.colorError))
-        }else{
-            holder.request.setTextColor(ThemeUtils.getThemeAttrColor(context, com.google.android.material.R.attr.colorControlNormal))
+        if (request.requestType == "all" && request.isBlocked == true) {
+            holder.request.setTextColor(
+                ThemeUtils.getThemeAttrColor(
+                    context,
+                    com.google.android.material.R.attr.colorError
+                )
+            )
+        } else {
+            holder.request.setTextColor(
+                ThemeUtils.getThemeAttrColor(
+                    context,
+                    com.google.android.material.R.attr.colorControlNormal
+                )
+            )
         }
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         holder.timestamp.text = sdf.format(Date(request.timestamp))
         holder.icon.setImageDrawable(AppUtils.getAppIcon(request.packageName))
+        holder.method = request.method
+        holder.urlString = request.urlString
+        holder.requestHeaders = request.requestHeaders
+        holder.responseCode = request.responseCode
+        holder.responseMessage = request.responseMessage
+        holder.responseHeaders = request.responseHeaders
     }
 
     override fun getItemCount(): Int {
@@ -87,6 +125,12 @@ class BlockedRequestsAdapter(
         var request: TextView
         var timestamp: TextView
         var icon: ImageView
+        var method: String? = null
+        var urlString: String? = null
+        var requestHeaders: String? = null
+        var responseCode = 0
+        var responseMessage: String? = null
+        var responseHeaders: String? = null
 
         init {
             appName = view.findViewById(R.id.app_name)
