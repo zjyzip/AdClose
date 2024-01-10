2f7308825b5e943915168a662e243787803a743e
zjyzip <zjyzip@github.com>
2024-01-10 18:00:43 +0800

    Data Optimize.

--- app/src/main/java/com/close/hook/ads/data/model/AppInfo.java
+++ app/src/main/java/com/close/hook/ads/data/model/AppInfo.java
@@ -7,138 +7,138 @@ import android.os.Parcelable;
 import java.util.Objects;
 
 public class AppInfo implements Parcelable {
-    private final String appName;
-    private final String packageName;
-    private final Drawable appIcon;
-    private final String versionName;
-    private final int versionCode;
-    private final Long firstInstallTime;
-    private final Long lastUpdateTime;
-    private final long size;
-    private final int targetSdk;
-    private final int isAppEnable;
-    private int isEnable;
+	private final String appName;
+	private final String packageName;
+	private final Drawable appIcon;
+	private final String versionName;
+	private final int versionCode;
+	private final Long firstInstallTime;
+	private final Long lastUpdateTime;
+	private final long size;
+	private final int targetSdk;
+	private final int isAppEnable;
+	private int isEnable;
 
-    public AppInfo(String appName, String packageName, Drawable appIcon, String versionName,
-				   int versionCode, Long firstInstallTime, Long lastUpdateTime, long size,
-                   int targetSdk, int isAppEnable, int isEnable) {
-        this.appName = appName;
-        this.packageName = packageName;
-        this.appIcon = appIcon;
-        this.versionName = versionName;
+	public AppInfo(String appName, String packageName, Drawable appIcon, String versionName, int versionCode,
+			Long firstInstallTime, Long lastUpdateTime, long size, int targetSdk, int isAppEnable, int isEnable) {
+		this.appName = appName;
+		this.packageName = packageName;
+		this.appIcon = appIcon;
+		this.versionName = versionName;
 		this.versionCode = versionCode;
 		this.firstInstallTime = firstInstallTime;
-        this.lastUpdateTime = lastUpdateTime;
-        this.size = size;
-        this.targetSdk = targetSdk;
-        this.isAppEnable = isAppEnable;
-        this.isEnable = isEnable;
-    }
+		this.lastUpdateTime = lastUpdateTime;
+		this.size = size;
+		this.targetSdk = targetSdk;
+		this.isAppEnable = isAppEnable;
+		this.isEnable = isEnable;
+	}
 
-    protected AppInfo(Parcel in) {
-        appName = in.readString();
-        packageName = in.readString();
-        appIcon = (Drawable) in.readValue(Drawable.class.getClassLoader());
-        versionName = in.readString();
+	protected AppInfo(Parcel in) {
+		appName = in.readString();
+		packageName = in.readString();
+		appIcon = (Drawable) in.readValue(Drawable.class.getClassLoader());
+		versionName = in.readString();
 		versionCode = in.readInt();
 		firstInstallTime = in.readLong();
-        lastUpdateTime = in.readLong();
-        size = in.readLong();
-        targetSdk = in.readInt();
-        isAppEnable = in.readInt();
-        isEnable = in.readInt();
-    }
+		lastUpdateTime = in.readLong();
+		size = in.readLong();
+		targetSdk = in.readInt();
+		isAppEnable = in.readInt();
+		isEnable = in.readInt();
+	}
 
-    public String getAppName() {
-        return appName;
-    }
+	public String getAppName() {
+		return appName;
+	}
 
-    public String getPackageName() {
-        return packageName;
-    }
+	public String getPackageName() {
+		return packageName;
+	}
 
-    public Drawable getAppIcon() {
-        return appIcon;
-    }
+	public Drawable getAppIcon() {
+		return appIcon;
+	}
 
-    public String getVersionName() {
-        return versionName;
-    }
+	public String getVersionName() {
+		return versionName;
+	}
+
 	public int getVersionCode() {
 		return versionCode;
 	}
 
-    public Long getFirstInstallTime() {
-        return firstInstallTime;
-    }
+	public Long getFirstInstallTime() {
+		return firstInstallTime;
+	}
 
-    public Long getLastUpdateTime() {
-        return lastUpdateTime;
-    }
+	public Long getLastUpdateTime() {
+		return lastUpdateTime;
+	}
 
-    public long getSize() {
-        return size;
-    }
+	public long getSize() {
+		return size;
+	}
 
-    public int getTargetSdk() {
-        return targetSdk;
-    }
+	public int getTargetSdk() {
+		return targetSdk;
+	}
 
-    public int getIsAppEnable() {
-        return isAppEnable;
-    }
+	public int getIsAppEnable() {
+		return isAppEnable;
+	}
 
-    public int getIsEnable() {
-        return isEnable;
-    }
+	public int getIsEnable() {
+		return isEnable;
+	}
 
-    public void setIsEnable(int isEnable) {
-        this.isEnable = isEnable;
-    }
+	public void setIsEnable(int isEnable) {
+		this.isEnable = isEnable;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (!(o instanceof AppInfo appInfo))
-            return false;
-        return Objects.equals(getPackageName(), appInfo.getPackageName());
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof AppInfo appInfo))
+			return false;
+		return Objects.equals(getPackageName(), appInfo.getPackageName());
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(getPackageName());
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(getPackageName());
+	}
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(appName);
-        dest.writeString(packageName);
-        dest.writeString(versionName);
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(appName);
+		dest.writeString(packageName);
+		dest.writeString(versionName);
 		dest.writeInt(versionCode);
 		dest.writeValue(appIcon);
-        dest.writeLong(firstInstallTime);
-        dest.writeLong(lastUpdateTime);
-        dest.writeLong(size);
-        dest.writeInt(targetSdk);
-        dest.writeInt(isAppEnable);
-        dest.writeInt(isEnable);
-    }
+		dest.writeLong(firstInstallTime);
+		dest.writeLong(lastUpdateTime);
+		dest.writeLong(size);
+		dest.writeInt(targetSdk);
+		dest.writeInt(isAppEnable);
+		dest.writeInt(isEnable);
+	}
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
-    public static final Creator<AppInfo> CREATOR = new Creator<AppInfo>() {
-        @Override
-        public AppInfo createFromParcel(Parcel in) {
-            return new AppInfo(in);
-        }
+	public static final Creator<AppInfo> CREATOR = new Creator<AppInfo>() {
+		@Override
+		public AppInfo createFromParcel(Parcel in) {
+			return new AppInfo(in);
+		}
 
-        @Override
-        public AppInfo[] newArray(int size) {
-            return new AppInfo[size];
-        }
-    };
+		@Override
+		public AppInfo[] newArray(int size) {
+			return new AppInfo[size];
+		}
+	};
 }
--- app/src/main/java/com/close/hook/ads/ui/viewmodel/AppsViewModel.java
+++ app/src/main/java/com/close/hook/ads/ui/viewmodel/AppsViewModel.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.LiveDataReactiveStreams;
-import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.close.hook.ads.data.model.AppInfo;
@@ -14,23 +13,24 @@ import com.close.hook.ads.data.repository.AppRepository;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class AppsViewModel extends AndroidViewModel {
+    public List<BlockedRequest> requestList = new ArrayList<>();
+    public List<AppInfo> appInfoList = new ArrayList<>();
 
-	public HashMap<String, Integer> checkHashMap = new HashMap<>();
-	public List<BlockedRequest> requestList = new ArrayList<>();
-	public List<AppInfo> appInfoList = new ArrayList<>();
-
-	private final AppRepository appRepository;
+    private final AppRepository appRepository;
     private final LiveData<List<AppInfo>> userAppsLiveData;
     private final LiveData<List<AppInfo>> systemAppsLiveData;
-	private final MutableLiveData<LoadState> loadState = new MutableLiveData<>();
-	private String currentSearchKeyword = "";
+    private final MutableLiveData<LoadState> loadState = new MutableLiveData<>();
+    private final MutableLiveData<String> errorLiveData = new MutableLiveData<>();
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private String currentSearchKeyword = "";
 
     public AppsViewModel(Application application) {
         super(application);
@@ -38,18 +38,35 @@ public class AppsViewModel extends AndroidViewModel {
         appRepository = new AppRepository(packageManager);
 
         userAppsLiveData = LiveDataReactiveStreams.fromPublisher(
-            appRepository.getInstalledUserApps()
-            .toFlowable(BackpressureStrategy.BUFFER)
-            .subscribeOn(Schedulers.io())
-            .distinctUntilChanged()
-            .onErrorReturnItem(Collections.emptyList()));
+                appRepository.getInstalledUserApps()
+                .toFlowable(BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> loadState.setValue(LoadState.LOADING))
+                .doOnError(error -> {
+                    loadState.setValue(LoadState.ERROR);
+                    errorLiveData.setValue(error.getMessage());
+                })
+                .onErrorReturnItem(Collections.emptyList())
+                .doOnNext(list -> loadState.setValue(LoadState.COMPLETE)));
 
         systemAppsLiveData = LiveDataReactiveStreams.fromPublisher(
-            appRepository.getInstalledSystemApps()
-            .toFlowable(BackpressureStrategy.BUFFER)
-            .subscribeOn(Schedulers.io())
-            .distinctUntilChanged()
-            .onErrorReturnItem(Collections.emptyList()));
+                appRepository.getInstalledSystemApps()
+                .toFlowable(BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(disposable -> loadState.setValue(LoadState.LOADING))
+                .doOnError(error -> {
+                    loadState.setValue(LoadState.ERROR);
+                    errorLiveData.setValue(error.getMessage());
+                })
+                .onErrorReturnItem(Collections.emptyList())
+                .doOnNext(list -> loadState.setValue(LoadState.COMPLETE)));
+    }
+
+    public void updateAppInfoList(List<AppInfo> newAppInfoList) {
+        this.appInfoList.clear();
+        this.appInfoList.addAll(newAppInfoList);
     }
 
     public LiveData<List<AppInfo>> getUserAppsLiveData() {
@@ -60,19 +77,33 @@ public class AppsViewModel extends AndroidViewModel {
         return systemAppsLiveData;
     }
 
-	public LiveData<LoadState> getLoadState() {
-		return loadState;
-	}
+    public LiveData<LoadState> getLoadState() {
+        return loadState;
+    }
 
-	public String getCurrentSearchKeyword() {
-		return currentSearchKeyword;
-	}
+    public LiveData<String> getErrorLiveData() {
+        return errorLiveData;
+    }
 
-	public void setCurrentSearchKeyword(String searchKeyword) {
-		this.currentSearchKeyword = searchKeyword;
-	}
+    public String getCurrentSearchKeyword() {
+        return currentSearchKeyword;
+    }
 
-	public enum LoadState {
-		LOADING, COMPLETE, ERROR
-	}
+    public void setCurrentSearchKeyword(String searchKeyword) {
+        this.currentSearchKeyword = searchKeyword;
+    }
+
+    public void clearRequests() {
+        requestList.clear();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.clear();
+    }
+
+    public enum LoadState {
+        LOADING, COMPLETE, ERROR
+    }
 }
