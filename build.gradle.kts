153b94770a0696c9b6db2f4133b5ae837deec727
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-02-02 09:04:40 +0800

    use ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.3.3"
-
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
 
 tasks.register<Delete>("clean") {
