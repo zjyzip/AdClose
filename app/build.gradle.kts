037836d57ef1cbd238a5ebec77cda48d43ea6a06
zjyzip <zjyzip@github.com>
2024-02-06 19:10:12 +0800

    adjust.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = calculateVersionCode()
-        versionName = "1.0.0"
+        versionName = "1.5.9"
 
         vectorDrawables {
             useSupportLibrary = true
