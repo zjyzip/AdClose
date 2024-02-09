07cd374403e947ab6499272581b123fb249f606e
zjyzip <zjyzip@github.com>
2024-02-09 09:17:12 +0800

    optimize.

--- app/src/main/java/com/close/hook/ads/ui/activity/BlockListActivity.kt
+++ app/src/main/java/com/close/hook/ads/ui/activity/BlockListActivity.kt
@@ -2,6 +2,7 @@ package com.close.hook.ads.ui.activity
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -11,6 +12,7 @@ import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -34,12 +36,12 @@ import kotlinx.coroutines.withContext
 import me.zhanghai.android.fastscroll.FastScrollerBuilder
 import java.io.IOException
 
-class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
+class BlockListActivity : BaseActivity() {
 
     private lateinit var binding: ActivityBlockListBinding
     private val viewModel by lazy { ViewModelProvider(this)[BlockListViewModel::class.java] }
     private lateinit var mAdapter: BlockListAdapter
-    private lateinit var mLayoutManager: LinearLayoutManager
+    private lateinit var mLayoutManager: RecyclerView.LayoutManager
     private val urlDao by lazy {
         UrlDatabase.getDatabase(this).urlDao
     }
@@ -66,9 +68,71 @@ class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
                 viewModel.blockList.addAll(it)
                 submitList()
             }
-
         }
+    }
 
+    private fun initView() {
+        mLayoutManager = LinearLayoutManager(this)
+        mAdapter = BlockListAdapter(this,
+            onRemoveUrl = { position ->
+                onRemoveUrl(position)
+            },
+            onEditUrl = { position ->
+                onEditUrl(position)
+            }
+        )
+        FastScrollerBuilder(binding.recyclerView).useMd2Style().build()
+        binding.recyclerView.apply {
+            adapter = mAdapter
+            layoutManager = mLayoutManager
+        }
+    }
+
+    private fun submitList() {
+        val newList = ArrayList<Item>().apply {
+            addAll(viewModel.blockList)
+        }
+        mAdapter.submitList(newList)
+    }
+
+    private fun onRemoveUrl(position: Int) {
+        val url = viewModel.blockList[position].url
+        CoroutineScope(Dispatchers.IO).launch {
+            urlDao.delete(url)
+            viewModel.blockList.removeAt(position)
+            withContext(Dispatchers.Main) {
+                submitList()
+            }
+        }
+    }
+
+    private fun onEditUrl(position: Int) {
+        val item = viewModel.blockList[position]
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.item_block_list_add, null)
+        val editText: TextInputEditText = dialogView.findViewById(R.id.editText)
+        editText.setText(item.url)
+        val type = dialogView.findViewById<EditText>(R.id.type)
+        type.setText(item.type)
+
+        MaterialAlertDialogBuilder(this).setTitle("Edit Rule")
+            .setView(dialogView)
+            .setNegativeButton(android.R.string.cancel, null)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                val newType = type.text.toString()
+                val newUrl = editText.text.toString()
+                CoroutineScope(Dispatchers.IO).launch {
+                    urlDao.delete(item.url)
+                    urlDao.insert(Url(newType, newUrl))
+                    withContext(Dispatchers.Main) {
+                        viewModel.blockList[position] = Item(newType, newUrl)
+                        submitList()
+                    }
+                }
+            }
+            .create().apply {
+                window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+                editText.requestFocus()
+            }.show()
     }
 
     @SuppressLint("InflateParams", "SetTextI18n")
@@ -77,15 +141,14 @@ class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
             backupSAFLauncher.launch("block_list.rule")
         }
         binding.restore.setOnClickListener {
-            restoreSAFLauncher.launch("*/*")
+            restoreSAFLauncher.launch(arrayOf("application/octet-stream"))
         }
         binding.clear.setOnClickListener {
             binding.editText.text = null
             submitList()
         }
         binding.add.setOnClickListener {
-            val dialogView =
-                LayoutInflater.from(this).inflate(R.layout.item_block_list_add, null, false)
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.item_block_list_add, null, false)
             val editText: TextInputEditText = dialogView.findViewById(R.id.editText)
             val type: MaterialAutoCompleteTextView = dialogView.findViewById(R.id.type)
             type.setText("URL")
@@ -96,79 +159,74 @@ class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
                     arrayOf("Host", "URL", "KeyWord")
                 )
             )
-            MaterialAlertDialogBuilder(this).apply {
-                setTitle("Add Rule")
-                setView(dialogView)
-                setNegativeButton(android.R.string.cancel, null)
-                setPositiveButton(android.R.string.ok) { _, _ ->
+            MaterialAlertDialogBuilder(this).setTitle("Add Rule")
+                .setView(dialogView)
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    val newType = type.text.toString()
+                    val newUrl = editText.text.toString()
                     CoroutineScope(Dispatchers.IO).launch {
-                        with(
-                            editText.text.toString()
-                                .replace(" ", "")
-                                .replace("\n", "")
-                        ) {
-                            if (this.isEmpty()) return@launch
-                            else if (viewModel.blockList.indexOf(
-                                    Item(type.text.toString(), this)
-                                ) != -1
-                            ) {
-                                withContext(Dispatchers.Main) {
-                                    Toast.makeText(
-                                        this@BlockListActivity,
-                                        "已存在",
-                                        Toast.LENGTH_SHORT
-                                    )
-                                        .show()
-                                }
-                            } else {
-                                urlDao.insert(Url(type.text.toString(), this))
-                                withContext(Dispatchers.Main) {
-                                    viewModel.blockList.add(
-                                        0,
-                                        Item(type.text.toString(), this@with)
-                                    )
-                                    submitList()
-                                }
-                            }
+                        urlDao.insert(Url(newType, newUrl))
+                        withContext(Dispatchers.Main) {
+                            viewModel.blockList.add(0, Item(newType, newUrl))
+                            submitList()
                         }
                     }
                 }
-            }.create().apply {
-                window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-                editText.requestFocus()
-            }.show()
+                .create().apply {
+                    window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+                    editText.requestFocus()
+                }.show()
+        }
+    }
+    
+    private fun initToolBar() {
+        binding.backbtn.setOnClickListener {
+            finish()
+        }
+    }
+    
+    private fun initClearHistory() {
+        binding.clearAll.setOnClickListener {
+            MaterialAlertDialogBuilder(this).setTitle("确定清除全部黑名单？")
+                .setNegativeButton(android.R.string.cancel, null)
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    CoroutineScope(Dispatchers.IO).launch {
+                        urlDao.deleteAll()
+                        viewModel.blockList.clear()
+                        withContext(Dispatchers.Main) {
+                            submitList()
+                        }
+                    }
+                }
+                .show()
         }
     }
 
     private fun search() {
-        val newList = ArrayList<Item>()
-        viewModel.blockList.forEach {
-            if (it.url.contains(binding.editText.text.toString()))
-                newList.add(it)
+        val searchText = binding.editText.text.toString()
+        if (searchText.isNotEmpty()) {
+            val filteredList = viewModel.blockList.filter { it.url.contains(searchText, ignoreCase = true) }
+            mAdapter.submitList(filteredList)
+        } else {
+            mAdapter.submitList(viewModel.blockList)
         }
-        mAdapter.submitList(newList)
     }
 
     private fun initEditText() {
         binding.editText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
+    
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                binding.clear.visibility = if (binding.editText.text.isNullOrEmpty())
-                    View.GONE
-                else
-                    View.VISIBLE
+                binding.clear.visibility = if (binding.editText.text.isNullOrEmpty()) View.GONE else View.VISIBLE
                 search()
             }
-
+    
             override fun afterTextChanged(s: Editable) {}
         })
+    
         binding.editText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                binding.editText.setFocusableInTouchMode(false)
-                binding.editText.isFocusable = false
-                binding.editText.setFocusableInTouchMode(true)
-                binding.editText.isFocusable = true
                 val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                 imm.hideSoftInputFromWindow(binding.editText.windowToken, 0)
                 true
@@ -176,118 +234,6 @@ class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
                 false
             }
         }
-    }
-
-    private fun initToolBar() {
-        binding.backbtn.setOnClickListener {
-            finish()
-        }
-    }
-
-    private fun initClearHistory() {
-        binding.clearAll.setOnClickListener {
-            if (viewModel.blockList.isEmpty())
-                return@setOnClickListener
-            MaterialAlertDialogBuilder(this).apply {
-                setTitle("确定清除全部黑名单？")
-                setNegativeButton(android.R.string.cancel, null)
-                setPositiveButton(android.R.string.ok) { _, _ ->
-                    CoroutineScope(Dispatchers.IO).launch {
-                        urlDao.deleteAll()
-                    }
-                    viewModel.blockList.clear()
-                    val newList = ArrayList<Item>()
-                    mAdapter.submitList(newList)
-                }
-                show()
-            }
-        }
-    }
-
-    private fun initView() {
-        mLayoutManager = LinearLayoutManager(this)
-        mAdapter = BlockListAdapter(this)
-        mAdapter.setCallBack(this)
-        FastScrollerBuilder(binding.recyclerView).useMd2Style().build()
-        binding.recyclerView.apply {
-            adapter = mAdapter
-            layoutManager = mLayoutManager
-        }
-    }
-
-    override fun onRemoveUrl(position: Int) {
-        val url = viewModel.blockList[position].url
-        CoroutineScope(Dispatchers.IO).launch {
-            urlDao.delete(url)
-        }
-        viewModel.blockList.removeAt(position)
-        submitList()
-    }
-
-    private fun submitList() {
-        val newList = ArrayList<Item>()
-        newList.addAll(viewModel.blockList)
-        mAdapter.submitList(newList)
-    }
-
-    @SuppressLint("InflateParams")
-    override fun onEditUrl(position: Int) {
-        val dialogView =
-            LayoutInflater.from(this).inflate(R.layout.item_block_list_add, null, false)
-        val editText: EditText = dialogView.findViewById(R.id.editText)
-        editText.setText(viewModel.blockList[position].url)
-        val type: MaterialAutoCompleteTextView = dialogView.findViewById(R.id.type)
-        type.setText(
-            viewModel.blockList[position].type.replace("host", "Host").replace("url", "URL")
-                .replace("keyword", "KeyWord")
-        )
-        type.setAdapter(
-            ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_dropdown_item,
-                arrayOf("Host", "URL", "KeyWord")
-            )
-        )
-        MaterialAlertDialogBuilder(this).apply {
-            setTitle("Edit Rule")
-            setView(dialogView)
-            setNegativeButton(android.R.string.cancel, null)
-            setPositiveButton(android.R.string.ok) { _, _ ->
-                CoroutineScope(Dispatchers.IO).launch {
-                    with(
-                        editText.text.toString()
-                            .replace(" ", "")
-                            .replace("\n", "")
-                    ) {
-                        if (this.isEmpty()) return@launch
-                        else if (viewModel.blockList.indexOf(
-                                Item(type.text.toString(), this)
-                            ) != -1
-                        ) {
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(
-                                    this@BlockListActivity,
-                                    "已存在",
-                                    Toast.LENGTH_SHORT
-                                )
-                                    .show()
-                            }
-                        } else {
-                            urlDao.delete(viewModel.blockList[position].url)
-                            urlDao.insert(Url(type.text.toString(), this))
-                            withContext(Dispatchers.Main) {
-                                viewModel.blockList.removeAt(position)
-                                viewModel.blockList.add(0, Item(type.text.toString(), this@with))
-                                submitList()
-                            }
-                        }
-                    }
-                }
-            }
-        }.create().apply {
-            window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-            editText.requestFocus()
-        }.show()
     }
 
     private fun prepareDataForExport(): List<String> {
@@ -299,43 +245,36 @@ class BlockListActivity : BaseActivity(), BlockListAdapter.CallBack {
     }
 
     private val restoreSAFLauncher =
-        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
             uri?.let { uri ->
-                val fileName = uri.path
-                if (fileName != null && fileName.endsWith(".rule")) {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        try {
-                            val newItems = contentResolver.openInputStream(uri)?.bufferedReader()?.useLines { lines ->
-                                lines.mapNotNull { line ->
-                                    val parts = line.split(",\\s*".toRegex()).map { it.trim() }
-                                    if (parts.size == 2) Item(parts[0], parts[1]) else null
-                                }
-                                .distinct()
-                                .sortedBy { it.url }
-                                .toList()
-                            } ?: listOf()
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        val newItems = contentResolver.openInputStream(uri)?.bufferedReader()?.useLines { lines ->
+                            lines.mapNotNull { line ->
+                                val parts = line.split(",\\s*".toRegex()).map { it.trim() }
+                                if (parts.size == 2) Item(parts[0], parts[1]) else null
+                            }
+                            .distinct()
+                            .sortedBy { it.url }
+                            .toList()
+                        } ?: listOf()
     
-                            newItems.forEach { item ->
-                                if (viewModel.blockList.indexOf(Item(item.type, item.url)) == -1) {
-                                    urlDao.insert(Url(item.type, item.url))
-                                    viewModel.blockList.add(0, Item(item.type, item.url))
-                                }
+                        newItems.forEach { item ->
+                            if (viewModel.blockList.indexOf(Item(item.type, item.url)) == -1) {
+                                urlDao.insert(Url(item.type, item.url))
+                                viewModel.blockList.add(0, Item(item.type, item.url))
                             }
-    
-                            withContext(Dispatchers.Main) {
-                                submitList()
-                                Toast.makeText(this@BlockListActivity, "导入成功", Toast.LENGTH_SHORT).show()
-                            }
-                        } catch (e: IOException) {
-                            withContext(Dispatchers.Main) {
-                                Toast.makeText(this@BlockListActivity, "导入失败", Toast.LENGTH_SHORT).show()
-                            }
-                            e.printStackTrace()
                         }
-                    }
-                } else {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        Toast.makeText(this@BlockListActivity, "请选择正确格式后缀的规则文件 (.rule)", Toast.LENGTH_SHORT).show()
+    
+                        withContext(Dispatchers.Main) {
+                            submitList()
+                            Toast.makeText(this@BlockListActivity, "导入成功", Toast.LENGTH_SHORT).show()
+                        }
+                    } catch (e: IOException) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(this@BlockListActivity, "导入失败", Toast.LENGTH_SHORT).show()
+                        }
+                        e.printStackTrace()
                     }
                 }
             }
--- app/src/main/java/com/close/hook/ads/ui/adapter/BlockListAdapter.kt
+++ app/src/main/java/com/close/hook/ads/ui/adapter/BlockListAdapter.kt
@@ -15,91 +15,74 @@ import com.close.hook.ads.R
 import com.close.hook.ads.data.model.Item
 import com.close.hook.ads.databinding.ItemBlockListBinding
 
-class BlockListAdapter(private val context: Context) :
-    ListAdapter<Item, BlockListAdapter.ViewHolder>(diffCallback),
-    PopupMenu.OnMenuItemClickListener {
-
-    private var callBack: CallBack? = null
-    fun setCallBack(callBack: CallBack) {
-        this.callBack = callBack
-    }
-
-    private var position = -1
+class BlockListAdapter(private val context: Context,
+                       private val onRemoveUrl: (Int) -> Unit,
+                       private val onEditUrl: (Int) -> Unit) :
+    ListAdapter<Item, BlockListAdapter.ViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val viewHolder = ViewHolder.from(parent)
-        viewHolder.itemView.setOnLongClickListener {
-            position = viewHolder.bindingAdapterPosition
-            val popup = PopupMenu(parent.context, it)
-            val inflater = popup.menuInflater
-            inflater.inflate(R.menu.menu_request, popup.menu)
-            popup.menu.findItem(R.id.block).title = "移除黑名单"
-            popup.setOnMenuItemClickListener(this@BlockListAdapter)
-            popup.show()
-            true
-        }
-        return viewHolder
+        val binding = ItemBlockListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding, context, onRemoveUrl, onEditUrl)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(currentList[position])
+        holder.bind(getItem(position))
     }
 
-    class ViewHolder(private val binding: ItemBlockListBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(
+        private val binding: ItemBlockListBinding,
+        private val context: Context,
+        private val onRemoveUrl: (Int) -> Unit,
+        private val onEditUrl: (Int) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root), PopupMenu.OnMenuItemClickListener {
+
+        init {
+            itemView.setOnLongClickListener {
+                showPopupMenu()
+                true
+            }
+        }
+
+        private fun showPopupMenu() {
+            PopupMenu(context, itemView).apply {
+                menuInflater.inflate(R.menu.menu_request, menu)
+                menu.findItem(R.id.block).title = "移除黑名单"
+                setOnMenuItemClickListener(this@ViewHolder)
+                show()
+            }
+        }
 
         fun bind(item: Item) {
-            binding.url.text = item.url
-            binding.type.text = item.type.replace("host", "Host").replace("url", "URL")
-                .replace("keyword", "KeyWord")
-        }
-
-        companion object {
-            fun from(parent: ViewGroup): ViewHolder {
-                val inflater = LayoutInflater.from(parent.context)
-                val binding = ItemBlockListBinding.inflate(inflater, parent, false)
-                return ViewHolder(binding)
+            with(binding) {
+                url.text = item.url
+                type.text = item.type.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() }
             }
         }
-    }
 
-    override fun onMenuItemClick(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.copy -> {
-                val clipboardManager =
-                    context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                ClipData.newPlainText("request", currentList[position].url)
-                    ?.let { clipboardManager.setPrimaryClip(it) }
-                Toast.makeText(context, "已复制: ${currentList[position].url}", Toast.LENGTH_SHORT)
-                    .show()
+        override fun onMenuItemClick(menuItem: MenuItem?): Boolean {
+            when (menuItem?.itemId) {
+                R.id.copy -> copyToClipboard()
+                R.id.block -> onRemoveUrl(adapterPosition)
+                R.id.edit -> onEditUrl(adapterPosition)
             }
-
-            R.id.block -> {
-                callBack?.onRemoveUrl(position)
-            }
-
-            R.id.edit -> {
-                callBack?.onEditUrl(position)
-            }
+            return true
         }
-        return false
-    }
 
-    interface CallBack {
-        fun onRemoveUrl(position: Int)
-        fun onEditUrl(position: Int)
+        private fun copyToClipboard() {
+            val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("request", binding.url.text)
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(context, "已复制: ${binding.url.text}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
-        val diffCallback = object : DiffUtil.ItemCallback<Item>() {
-            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
-                return oldItem == newItem
-            }
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Item>() {
+            override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean =
+                oldItem.url == newItem.url && oldItem.type == newItem.type
 
-            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
-                return oldItem == newItem
-            }
+            override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean =
+                oldItem == newItem
         }
     }
-
 }
