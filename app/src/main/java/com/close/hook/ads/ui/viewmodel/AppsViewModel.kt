3e6a8d0450dd483c2adb6ea8a04091f50c87e4fb
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-02-20 09:00:57 +0800

    optimize

---
+++ app/src/main/java/com/close/hook/ads/ui/viewmodel/AppsViewModel.kt
@@ -0,0 +1,59 @@
+package com.close.hook.ads.ui.viewmodel
+
+import android.app.Application
+import android.content.pm.PackageManager
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.close.hook.ads.data.model.AppInfo
+import com.close.hook.ads.data.model.FilterBean
+import com.close.hook.ads.data.model.BlockedRequest
+import com.close.hook.ads.data.repository.AppRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AppsViewModel(application: Application) : AndroidViewModel(application) {
+    var requestList = ArrayList<BlockedRequest>()
+    var appInfoList = ArrayList<AppInfo>()
+    var filterList = ArrayList<AppInfo>()
+    private val appRepository: AppRepository = AppRepository(application.packageManager)
+    private val _userAppsLiveData = MutableLiveData<List<AppInfo>>()
+    val userAppsLiveData: LiveData<List<AppInfo>> = _userAppsLiveData
+    private val _systemAppsLiveData = MutableLiveData<List<AppInfo>>()
+    val systemAppsLiveData: LiveData<List<AppInfo>> = _systemAppsLiveData
+    private val _errorLiveData = MutableLiveData<String>()
+    val errorLiveData: LiveData<String> = _errorLiveData
+    var currentSearchKeyword = ""
+    lateinit var sortList: ArrayList<String>
+    lateinit var filterBean: FilterBean
+    var isFilter = false
+
+    init {
+        loadUserApps()
+        loadSystemApps()
+    }
+
+    private fun loadUserApps() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val apps = appRepository.getInstalledUserApps()
+                _userAppsLiveData.postValue(apps)
+            } catch (e: Exception) {
+                _errorLiveData.postValue(e.message)
+            }
+        }
+    }
+
+    private fun loadSystemApps() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val apps = appRepository.getInstalledSystemApps()
+                _systemAppsLiveData.postValue(apps)
+            } catch (e: Exception) {
+                _errorLiveData.postValue(e.message)
+            }
+        }
+    }
+
+}
