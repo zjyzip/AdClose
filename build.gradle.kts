70722f6080582b23c09c84ecc2aa6cfe5220a55f
Reese_zip <76038670+zjyzip@users.noreply.github.com>
2024-03-08 08:23:54 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,9 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.3.3"
-    id("com.google.devtools.ksp") version "1.9.22-1.0.18" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
 }
 
 tasks.register<Delete>("clean") {
