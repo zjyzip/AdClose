860e05abd7e4cdb5070928c01de57d5cb6dc12e0
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-03-29 23:07:31 +0800

    optimize AppsFragment

--- app/src/main/java/com/close/hook/ads/util/OnClearClickListener.kt
+++ app/src/main/java/com/close/hook/ads/util/OnClearClickListener.kt
@@ -3,7 +3,7 @@ package com.close.hook.ads.util
 import com.close.hook.ads.data.model.FilterBean
 
 interface OnClearClickListener {
-    fun onClearAll()
+    fun onClearAll() {}
     fun search(keyWord: String?) {}
     fun updateSortList(filterBean: FilterBean, keyWord: String, isReverse: Boolean)
 }
