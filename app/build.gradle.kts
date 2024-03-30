023634e9a1cbe95234059ae76e7904583562bffb
zjyzip <zjyzip@github.com>
2024-03-31 03:53:21 +0800

    optimize.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -135,6 +135,8 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
     implementation("androidx.room:room-runtime:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    runtimeOnly("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
 
 
