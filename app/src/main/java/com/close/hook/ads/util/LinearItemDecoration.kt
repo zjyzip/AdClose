871fac8a800d7e6f8f65764502e18aba3bd9c682
bggRGjQaUbCoE <githubaccount56556@proton.me>
2024-03-30 10:56:32 +0800

    perf: add header

---
+++ app/src/main/java/com/close/hook/ads/util/LinearItemDecoration.kt
@@ -0,0 +1,18 @@
+package com.close.hook.ads.util
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class LinearItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        outRect.top = space
+    }
+
+}
