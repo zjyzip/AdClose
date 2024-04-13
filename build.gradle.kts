fa22e0ba245bc77a03b3b22d21e985a3ee3306a1
Reese_zip <76038670+zjyzip@users.noreply.github.com>
2024-04-14 01:25:14 +0800

    更新 build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("com.android.library") version "8.3.1" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.3.3"
     id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
