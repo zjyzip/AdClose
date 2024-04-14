a2458f6ee7574f6b0d0b782563e531fdadc4dc70
Reese_zip <76038670+zjyzip@users.noreply.github.com>
2024-04-14 09:23:55 +0800

    更新 UrlDatabase.kt

--- app/src/main/java/com/close/hook/ads/data/database/UrlDatabase.kt
+++ app/src/main/java/com/close/hook/ads/data/database/UrlDatabase.kt
@@ -17,12 +17,18 @@ abstract class UrlDatabase : RoomDatabase() {
         @Volatile
         private var instance: UrlDatabase? = null
 
-        private val MIGRATION_1_3: Migration = object : Migration(1, 3) {
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("CREATE TABLE url_info_new (id INTEGER NOT NULL, url TEXT NOT NULL, type TEXT NOT NULL DEFAULT 'url', PRIMARY KEY(id))")
-                db.execSQL("INSERT INTO url_info_new (id, url, type) SELECT id, url, 'url' FROM url_info")
+                db.execSQL("CREATE TABLE url_info_new (id INTEGER NOT NULL, url TEXT NOT NULL, PRIMARY KEY(id))")
+                db.execSQL("INSERT INTO url_info_new (id, url) SELECT id, url FROM url_info")
                 db.execSQL("DROP TABLE url_info")
                 db.execSQL("ALTER TABLE url_info_new RENAME TO url_info")
+            }
+        }
+
+        private val MIGRATION_2_3: Migration = object : Migration(2, 3) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE url_info ADD COLUMN type TEXT NOT NULL DEFAULT 'url'")
             }
         }
 
@@ -33,7 +39,7 @@ abstract class UrlDatabase : RoomDatabase() {
                     UrlDatabase::class.java,
                     "url_database"
                 )
-                .addMigrations(MIGRATION_1_3)
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
                 .build().also {
                     instance = it
                 }
