0e5c7823b0448b25857988560a7b74b61b1d8c53
zjyzip <zjyzip@github.com>
2024-04-19 23:15:02 +0800

    tweak.

--- app/src/main/java/com/close/hook/ads/service/AidlService.kt
+++ app/src/main/java/com/close/hook/ads/service/AidlService.kt
@@ -1,11 +1,17 @@
 package com.close.hook.ads.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.pm.ServiceInfo;
 import android.os.IBinder
 import android.os.MemoryFile
 import android.os.ParcelFileDescriptor
 import android.util.Log
+import com.close.hook.ads.R
 import com.close.hook.ads.BlockedBean
 import com.close.hook.ads.IBlockedStatusProvider
 import com.close.hook.ads.data.DataSource
@@ -45,7 +51,7 @@ class AidlService : Service() {
             val getFileDescriptorMethod = MemoryFile::class.java.getDeclaredMethod("getFileDescriptor")
             getFileDescriptorMethod.isAccessible = true
             val rawFileDescriptor = getFileDescriptorMethod.invoke(memoryFile) as FileDescriptor
-    
+
             return ParcelFileDescriptor.dup(rawFileDescriptor).also {
                 memoryFile.close()
             }
@@ -55,5 +61,25 @@ class AidlService : Service() {
         } finally {
             memoryFile?.close()
         }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startServiceForeground()
+    }
+
+    private fun startServiceForeground() {
+        val channelID = "com.close.hook.ads.service"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(channelID, "AIDL Service Channel", NotificationManager.IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
+
+        val notification = Notification.Builder(this, channelID)
+            .setContentTitle("Foreground Service Running")
+            .setContentText("Blocking ad requests")
+            .setSmallIcon(R.drawable.notice_smallicon)
+            .build()
+
+        startForeground(1, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
     }
 }
