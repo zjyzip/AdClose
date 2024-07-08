8200841f0264c7d032f0c3602c2a1f2886a2fede
zjyzip <zjyzip@github.com>
2024-07-08 14:53:18 +0800

    update dep.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("dev.rikka.tools.materialthemebuilder") version "1.3.3"
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
     id("dev.rikka.tools.autoresconfig") version "1.2.2" apply false
 }
 
