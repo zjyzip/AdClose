9921d2ffae6582f8fa779fd00a14f20eab545950
Reese_zip <76038670+zjyzip@users.noreply.github.com>
2025-02-20 01:36:16 +0800

    tweak HomeFragment.

--- app/src/main/java/com/close/hook/ads/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/close/hook/ads/ui/fragment/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.close.hook.ads.ui.fragment
 
 import android.annotation.SuppressLint
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -21,7 +23,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initToolBar()
         initInfo()
     }
@@ -30,39 +31,62 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     private fun initInfo() {
         val context = requireContext()
         val isActivated = MainActivity.isModuleActivated()
+        val primaryOrErrorColor = ThemeUtils.getThemeAttrColor(
+            context,
+            if (isActivated) com.google.android.material.R.attr.colorPrimary
+            else com.google.android.material.R.attr.colorError
+        )
+
         binding.apply {
-            val primaryOrErrorColor = ThemeUtils.getThemeAttrColor(
-                context,
-                if (isActivated) com.google.android.material.R.attr.colorPrimary
-                else com.google.android.material.R.attr.colorError
-            )
             status.setCardBackgroundColor(primaryOrErrorColor)
-
-            val statusIconDrawable = ContextCompat.getDrawable(
-                context,
-                if (isActivated) R.drawable.ic_round_check_circle_24
-                else R.drawable.ic_about
+            statusIcon.setImageDrawable(
+                ContextCompat.getDrawable(
+                    context,
+                    if (isActivated) R.drawable.ic_round_check_circle_24 else R.drawable.ic_about
+                )
             )
-            statusIcon.setImageDrawable(statusIconDrawable)
-
             statusTitle.text = getString(if (isActivated) R.string.activated else R.string.not_activated)
             statusSummary.text = getString(R.string.version_format, BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
 
+            setSystemInfo(context)
+            setHyperLinks()
+        }
+    }
+
+    private fun setSystemInfo(context: Context) {
+        val contentResolver = context.contentResolver
+
+        binding.apply {
             androidVersionValue.text = Build.VERSION.RELEASE
             sdkVersionValue.text = Build.VERSION.SDK_INT.toString()
-            androidIdValue.text = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-            brandValue.text = Build.BRAND
-            modelValue.text = Build.MODEL
-            skuValue.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) Build.SKU else ""
-            typeValue.text = Build.TYPE
-            fingerValue.text = Build.FINGERPRINT
 
-            val linkMovementMethod = LinkMovementMethod.getInstance()
+            androidIdValue.text = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
+
+            brandValue.text = Build.MANUFACTURER
+            modelValue.text = Build.MODEL
+
+            skuValue.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) Build.SKU else ""
+
+            typeValue.text = when {
+                Build.TYPE == "user" -> "Release"
+                Build.TYPE == "userdebug" -> "Debug"
+                Build.TYPE == "eng" -> "Engineering"
+                else -> Build.TYPE
+            }
+
+            fingerValue.text = Build.FINGERPRINT
+        }
+    }
+
+    private fun setHyperLinks() {
+        val linkMovementMethod = LinkMovementMethod.getInstance()
+        binding.apply {
             viewSource.movementMethod = linkMovementMethod
             viewSource.text = HtmlCompat.fromHtml(
                 getString(R.string.about_view_source_code, "<b><a href=\"https://github.com/zjyzip/AdClose\">GitHub</a></b>"),
                 HtmlCompat.FROM_HTML_MODE_LEGACY
             )
+            
             feedback.movementMethod = linkMovementMethod
             feedback.text = HtmlCompat.fromHtml(
                 getString(R.string.join_telegram_channel, "<b><a href=\"https://t.me/AdClose\">Telegram</a></b>"),
@@ -76,15 +100,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
             title = getString(R.string.app_name)
             inflateMenu(R.menu.menu_home)
             setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.about -> requireContext().startActivity(
-                        Intent(
-                            requireContext(),
-                            AboutActivity::class.java
-                        )
-                    )
+                if (it.itemId == R.id.about) {
+                    startActivity(Intent(requireContext(), AboutActivity::class.java))
                 }
-                return@setOnMenuItemClickListener true
+                true
             }
         }
     }
