c7a2212fc7894549e073719ce65591d5f6a7c9b8
zjyzip <zjyzip@users.noreply.github.com>
2026-02-06 15:09:49 +0800

    fix build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.materialThemeBuilder)
+    alias(libs.plugins.materialThemeBuilder) apply false 
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.autoresconfig) apply false
     alias(libs.plugins.kotlin.plugin.serialization) apply false
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
